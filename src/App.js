577ebf2476543bbcdb2b4c4af0235e60ba28b0a2
Krishna Dhingra <dkrishna1608@gmail.com>
2021-08-20 03:17:20 +0530

    Creating the home contact component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Navbar from './components/Navbar/index.js'
 import First_Home_Component from './components/First_Home_Component/index.js'
 import Footer from './components/Footer/index.js'
+import Home_Contact from './components/Home_Contact/index.js'
 import './index.css'
 function App() {
   return (
     <div className="App">
+
       <Navbar></Navbar>
+
       <div className="container">
+
         <First_Home_Component></First_Home_Component>
+        <Home_Contact></Home_Contact>
         <Footer></Footer>
       </div>
 
---
+++ src/components/Home_Contact/index.js
@@ -0,0 +1,13 @@
+import './home_contact.css'
+function Home_Contact() {
+  return (
+    <div className="home_contact">
+        <div className="home_contact_text">
+            I'll love to make something for you or your company
+        </div>
+        <button className="home_contact_button">Hire Me</button>
+    </div>
+  );
+}
+
+export default Home_Contact;
