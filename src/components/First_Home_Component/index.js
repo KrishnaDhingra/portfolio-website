31c364613f55a11219795b2a935caeb0c8eae63b
Krishna Dhingra <dkrishna1608@gmail.com>
2021-08-20 01:48:46 +0530

    Creating the first home component

--- src/components/First_Home_Component/index.js
+++ src/components/First_Home_Component/index.js
@@ -1,11 +1,23 @@
 import './first_home_component.css'
+
 function First_Home_Component() {
   return (
     <div className="first_home_component">
-        <p>Hello worlda;ldfja;ldjdas;</p>
-        <h2>l;afja;lfjka;lfjkhell world my name iskrishna lkadflj </h2>
+
+      <div className="home_top_container">
+        <div className="name">krishna</div>
+      </div>
+
+      <div className="home_bottom_container">
+        <div className="home_text_container">
+          <div className="home_text_left">A digital studio creating experiences that work, for the people who matter.</div>
+          <div className="home_text_right"></div>
+        </div>
+        <button className="home_button">My Work</button>
+      </div>
     </div>
-  );
+
+  )
 }
 
 export default First_Home_Component;
