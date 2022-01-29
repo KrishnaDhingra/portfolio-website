03beeaf5c8fc8076b082e85af00cb7a5f8866711
Krishna Dhingra <dkrishna1608@gmail.com>
2022-01-29 09:22:11 +0530

    Making custom hook for checking contact info

---
+++ src/components/Contact/useForm.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+const useForm = (validate, name, email, subject, message) => {
+
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    let values = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message
+    }
+    setErrors(validate(values));
+  };
+
+  useEffect(() => {
+      if(Object.keys(errors).length === 0) {
+        console.log("Everything is good")
+      }
+    }, [errors]);
+
+  return { handleSubmit, errors };
+};
+
+export default useForm;
