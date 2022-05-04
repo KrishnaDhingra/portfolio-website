0adf3c6d988e82ea310d55ec739e63f80483fc46
Krishna Dhingra <dkrishna1608@gmail.com>
2022-05-05 01:36:54 +0530

    Changing the code of the gmail button

--- src/components/Home_Contact/index.js
+++ src/components/Home_Contact/index.js
@@ -1,25 +1,23 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react'
 import './home_contact.css'
 import { Link } from 'react-router-dom'
 import Aos from 'aos'
 
 function Home_Contact() {
-
-  useEffect(() =>{
-    Aos.init({duration: 1000, offset: 300});
+  useEffect(() => {
+    Aos.init({ duration: 1000, offset: 100 })
   }, [])
-  
 
   return (
     <div className="home_contact">
-        <div className="home_contact_text" data-aos="fade-up">
-            I'd love to make something for you or your company
-        </div>
-        <Link to="/contact">
-          <button className="home_contact_button">Hire Me</button>
-        </Link>
+      <div className="home_contact_text" data-aos="fade-up">
+        I'd love to make something for you or your company
+      </div>
+      <Link to="/contact">
+        <button className="home_contact_button">Hire Me</button>
+      </Link>
     </div>
-  );
+  )
 }
 
-export default Home_Contact;
+export default Home_Contact
